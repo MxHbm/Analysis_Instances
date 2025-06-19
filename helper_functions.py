06557743588e80ae2dd8c5271d6e2073a87a06eb
mxhbmnn <maximilian.hubmann@mailbox.tu-dresden.de>
2025-06-19 18:08:22 +0200

    addded code to transfrom txt insatnces to josn files

--- helper_functions.py
+++ helper_functions.py
@@ -146,6 +146,55 @@ def extract_customer_information(filtered_customers:pd.DataFrame, customer:int) 
 
     return nodes
 
+def write_json_file_transformation(instance:str,
+                                    perm:list[int],
+                                    filtered_data,
+                                    file_path)  -> None:
+    
+     
+    filename = f"{file_path}/{instance}.json"
+
+    vehicles_json = get_vehicle_dataframe(filtered_data["instance"])
+
+    nodes_json = []
+    for customer in perm:
+        
+        nodes = extract_customer_information(filtered_data["customers"], customer)
+
+        node_items = []
+        for _, single_demand in filtered_data["single_demands"][filtered_data["single_demands"]["Customer ID"] == str(customer)].iterrows():
+            refiltered_items = filtered_data["items"][filtered_data["items"]["Type"] == single_demand["Type"]]
+
+            if not refiltered_items.empty:
+                first_item = refiltered_items.iloc[0]  # Safe indexing
+
+                fragile_output = "Fragile" if first_item["Fragility"] == 1 else "None"
+
+                node_items.append({
+                    "Quantity": int(single_demand["Quantity"]),
+                    "Weight": float(first_item["Mass"]),
+                    "Length": int(first_item["Length"]),
+                    "Width": int(first_item["Width"]),
+                    "Height": int(first_item["Height"]),
+                    "Volume": int(first_item["Volume"]),
+                    "Fragility": fragile_output,
+                    "EnableHorizontalRotation": int(True),
+                    "Rotated": "None"
+                })
+
+        nodes.update({"Items": node_items})
+        nodes_json.append(nodes)
+
+    name_in_file = filename.split("/")[-1].split(".")[0]
+    data = {
+        "Name": name_in_file,
+        "Vehicles": vehicles_json,
+        "Nodes": nodes_json
+    }
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4)
+
 def write_json_file(instance:str,
                     num_customers:int,
                     j:int,
@@ -285,6 +334,54 @@ def generate_instances(instance:str, df:pd.DataFrame,
                 total_created += 1
 
     return total_created
+
+
+def transform_instances(instance:str, df:pd.DataFrame,
+                       aggregate_demands:pd.DataFrame,
+                       single_demands:pd.DataFrame,
+                       items:pd.DataFrame,
+                       customers:pd.DataFrame,
+                       file_path) -> int:
+    '''
+        Generate train instances with specific customer routes and demands
+    Args:       
+        instance (str): Name of the instance
+        df (pd.DataFrame): Instance dataset
+        aggregate_demands (pd.DataFrame): Aggregate demands dataset
+        single_demands (pd.DataFrame): Single demands dataset
+        items (pd.DataFrame): Items dataset
+    Returns:
+        int: Number of instances created
+    '''
+
+    # Create dict with filtered dataframes
+    filtered_data = get_filtered_data(instance, df, aggregate_demands, single_demands, items, customers)
+
+    # Calculate bounds for number of customers
+    max_customers = filtered_data["instance"]["Number of Customers"].values[0]
+
+    #Create list with dummy values for customers
+    numbers = list(range(1, max_customers + 1))
+
+    #Upper Bounds limit for number of customers 
+    max_weight = filtered_data["instance"]["Vehicle Capacity"].values[0]
+    max_volume = filtered_data["instance"]["Cargo Length"].values[0] * filtered_data["instance"]["Cargo Width"].values[0] * filtered_data["instance"]["Cargo Height"].values[0] 
+
+    # Cache dicts for aggregated demand to avoid repeated lookups
+    agg_volume_dict = dict(zip(filtered_data["agg_demands"]["Customer ID"].astype(int),
+                           filtered_data["agg_demands"]["Agg Volume"]))
+    agg_mass_dict = dict(zip(filtered_data["agg_demands"]["Customer ID"].astype(int),
+                         filtered_data["agg_demands"]["Agg Mass"]))
+
+    #Define range of possible number of customers
+    perm = numbers
+    #Create instances with random number of customers
+
+    perm.insert(0, 0) #Add depot at the beginning, if feasible
+    
+
+    write_json_file_transformation(instance, perm, filtered_data, file_path)
+
 
 
 def plot_boxplot(data:pd.DataFrame, column:str, order: list) -> None: 
